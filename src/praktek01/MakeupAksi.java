50eba1020fcb0724a5f0035d85e5208de925bc4d
Dita Annisaftia Rizky <32301025+Ditaannisaftiarizky@users.noreply.github.com>
2018-03-28 20:52:19 +0800

    revisi method main

--- src/praktek01/MakeupAksi.java
+++ src/praktek01/MakeupAksi.java
@@ -12,12 +12,31 @@ package praktek01;
 public class MakeupAksi {
     public static void main(String[] args) {
         Makeup Wardah = new Makeup();
+        Makeup Maybelline = new Makeup ();
+        Makeup Purbasari = new Makeup();
         
         Wardah.merek="Wardah";
         Wardah.produk="Lipstik";
+        Wardah.bahan="Matte";
+        Wardah.warna="Oh so Nude";
         Wardah.harga="Rp. 50000";
         
+        Maybelline.merek="Maybelline";
+        Maybelline.produk="Lipstik";
+        Maybelline.bahan="Matte";
+        Maybelline.warna="Orange Plum";
+        Maybelline.harga="Rp. 65000";
+        
+        Purbasari.merek="Purbasari";
+        Purbasari.produk="Lipstik";
+        Purbasari.bahan="Matte";
+        Purbasari.warna="Red";
+        Purbasari.harga="Rp. 45000";
+        
         Wardah.cetakInfo();
+        Maybelline.cetakInfo();
+        Purbasari.cetakInfo();
+       
        
         
     }
